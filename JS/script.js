d97f9cfe570c6b5efce3fe1c8254a33d3b1231b9
Francis <f.ehmeir@gmail.com>
2018-09-14 09:40:24 +0200

    P1

---
+++ JS/script.js
@@ -0,0 +1,23 @@
+var angle = 0;
+var metal;
+var lambo;
+
+function preload() {
+	metal = loadImage('../Car/Texture.png');
+	lambo = loadModel('../Car/Aventador/Avent.obj');
+}
+
+function setup() {
+	createCanvas(100, 100, WEBGL);
+}
+
+function draw() {
+	background(200);
+	//ambientLight(255, 0, 255);
+	//directionalLight(255, 255, 255, 0, 0, 1);
+	rotateY(angle * 1.3);
+	translate(0, 0, 0);
+	texture(metal);
+	model(lambo);
+	angle += 0.03;
+}
